225582f00824cd49c6621b3eec7c0c2647b55d7b
Rico P <MailToRico@googlemail.com>
2012-08-31 12:58:09 +0200

    first demo

---
+++ compiled.js
@@ -0,0 +1,107 @@
+
+
+
+
+ function __error(message, file, line) {
+  throw new Error(message + "(" + file + ":" + line + ")");
+ }
+do { if(!(64 === 16 * 4)) { __error("assertion failed: " + "CUBE_WIDTH === FACE_WIDTH * 4" + " = " + (64 === 16 * 4), "src/pixelops.js", 8); } } while(false);
+
+var Funkycube;
+
+!function() {
+"use strict";
+ function Point(x,y) {
+  this.x = x;
+  this.y = y;
+  this.toString = function() {
+   return "[" + x + ", " + y + "]";
+  };
+ }
+ Funkycube = function() {
+  var canvas = document.createElement("canvas");
+  canvas.width = canvas.height = 64;
+  var ctx = canvas.getContext("2d");
+  function getCanvasCoordinate(face, x, y) {
+   do { if(!(face >= 0 && face <= 5)) { __error("assertion failed: " + "face >= 0 && face <= 5" + " = " + (face >= 0 && face <= 5), "src/pixelops.js", 52); } } while(false);
+   if(x < 0) {
+    switch(face) {
+     case 0:
+     return getCanvasCoordinate(1, y, -x-1);
+     case 1:
+     return getCanvasCoordinate(5, -x-1, 16 - 1 - y);
+     case 2:
+     return getCanvasCoordinate(1, x + 16, y);
+     case 3:
+     return getCanvasCoordinate(2, x + 16, y);
+     case 4:
+     return getCanvasCoordinate(1, 16 - 1 - y, 16 + x);
+     case 5:
+     return getCanvasCoordinate(1, -x-1 ,16 - 1 - y);
+     default: throw new RangeError();
+    }
+   }
+   if(y < 0) {
+    switch(face) {
+     case 0:
+     return getCanvasCoordinate(5, x, 16 + y);
+     case 1:
+     return getCanvasCoordinate(0, -y ,x);
+     case 2:
+     return getCanvasCoordinate(0, x, 16 + y);
+     case 3:
+     return getCanvasCoordinate(0, 16 + y, 16 - x);
+     case 4:
+     return getCanvasCoordinate(2, x, 16 + y);
+     case 5:
+     return getCanvasCoordinate(4, x, 16 + y);
+     default: throw new RangeError();
+    }
+   }
+   if(x >= 16) {
+    switch(face) {
+     case 0:
+     return getCanvasCoordinate(3, 16 - y, x - 16);
+     case 1:
+     return getCanvasCoordinate(2, x - 16, y);
+     case 2:
+     return getCanvasCoordinate(3, x - 16, y);
+     case 3:
+     return getCanvasCoordinate(5, 2*16 -1 - x, 16 - y);
+     case 4:
+     return getCanvasCoordinate(3, y, 2*16 -1 - x);
+     case 5:
+     return getCanvasCoordinate(3, 2*16 -1 - x, 16 - y);
+     default: throw new RangeError();
+    }
+   }
+   if(y >= 16) {
+    switch(face) {
+     case 0:
+     return getCanvasCoordinate(2, x, y - 16);
+     case 1:
+     return getCanvasCoordinate(4, y - 16, 16 - x - 1);
+     case 2:
+     return getCanvasCoordinate(4, x, y - 16);
+     case 3:
+     return getCanvasCoordinate(4, 2*16 -1 - y ,x);
+     case 4:
+     return getCanvasCoordinate(5, x, y - 16);
+     case 5:
+     return getCanvasCoordinate(0, x, y - 16);
+     default: throw new RangeError();
+    }
+   }
+   switch(face) {
+    case 0: return new Point(x + 16 , y);
+    case 1: return new Point(x , y + 16);
+    case 2: return new Point(x + 16 , y + 16);
+    case 3: return new Point(x + 16*2, y + 16);
+    case 4: return new Point(x + 16 , y + 16*2);
+    case 5: return new Point(x + 16 , y + 16*3);
+    default: throw new RangeError();
+   }
+  }
+  this.getCanvasCoordinate = getCanvasCoordinate;
+ };
+}();
